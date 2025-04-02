336ed5b555479a9e8badd0297dc3143a73d90389
Erik Shafer <erik.shafer@gmail.com>
2025-04-02 00:28:01 -0500

    now we're using Wolverine endpoints!

--- src/Catalog/Catalog.Api/Program.cs
+++ src/Catalog/Catalog.Api/Program.cs
@@ -28,7 +28,6 @@ builder.Host.UseWolverine(opts =>
 });
 
 builder.Services.AddOpenApi();
-
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -49,34 +48,4 @@ app.MapWolverineEndpoints();
 
 app.UseHttpsRedirection(); // TODO: Need?
 
-#region stock_dotnet_weather_minimal_api
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
-// app.Run();
-
-#endregion
-
 return await app.RunOaktonCommands(args);
-
-internal sealed record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
