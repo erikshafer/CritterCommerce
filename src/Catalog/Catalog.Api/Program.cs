5537e9df7e31406498af7b82095665d599468bc0
Erik Shafer <erik.shafer@gmail.com>
2025-04-02 00:17:53 -0500

    added Wolverine to solution, but same API shape

--- src/Catalog/Catalog.Api/Program.cs
+++ src/Catalog/Catalog.Api/Program.cs
@@ -1,18 +1,55 @@
+using Marten;
+using Oakton;
+using Oakton.Resources;
+using Wolverine;
+using Wolverine.Http;
+using Wolverine.Marten;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+// Adding Marten for persistence
+builder.Services.AddMarten(opts =>
+    {
+        opts.Connection(builder.Configuration.GetConnectionString("Marten"));
+        opts.DatabaseSchemaName = "catalog";
+    })
+    .IntegrateWithWolverine();
+
+builder.Services.AddResourceSetupOnStartup();
+
+// Wolverine usage is required for WolverineFx.Http
+builder.Host.UseWolverine(opts =>
+{
+    // This middleware will apply to the HTTP endpoints as well
+    opts.Policies.AutoApplyTransactions();
+
+    // Setting up the outbox on all locally handled background tasks
+    opts.Policies.UseDurableLocalQueues();
+});
+
 builder.Services.AddOpenApi();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddWolverineHttp();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+// Let's add in Wolverine HTTP endpoints to the routing tree
+app.MapWolverineEndpoints();
+
+app.UseHttpsRedirection(); // TODO: Need?
+
+#region stock_dotnet_weather_minimal_api
 
 var summaries = new[]
 {
@@ -33,9 +70,13 @@ app.MapGet("/weatherforecast", () =>
     })
     .WithName("GetWeatherForecast");
 
-app.Run();
+// app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+#endregion
+
+return await app.RunOaktonCommands(args);
+
+internal sealed record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
