dc0fb7fecbc74deae11179434fedba52adb114ac
Erik Shafer <erik.shafer@gmail.com>
2025-04-14 20:10:22 -0500

    added transactional Create endpoint, non-minimal

--- src/Catalog/Catalog.Api/CreateItemCommandHandler.cs
+++ src/Catalog/Catalog.Api/CreateItemCommandHandler.cs
@@ -1,3 +1,4 @@
+using JasperFx.Core;
 using Wolverine.Attributes;
 using Wolverine.Http;
 using Wolverine.Persistence;
@@ -31,19 +32,20 @@ public class ItemCreatedHandler
 
 public static class ItemEndpoints
 {
-    // [Transactional]
-    // [WolverinePost("/items/create"), EmptyResponse]
-    // public static (ItemCreated, Insert<Item>) Post(CreateItemCommand command)
-    // {
-    //     // Create a new Item entity
-    //     var item = new Item
-    //     {
-    //         Name = command.Name,
-    //         Id = CombGuidIdGeneration.NewGuid()
-    //     };
-    //
-    //     return (new ItemCreated(item.Id), Storage.Insert(item));
-    // }
+    [Transactional]
+    [WolverinePost("/api/items/create"), AlwaysPublishResponse]
+    public static (ItemCreated, Insert<Item>) Post(CreateItemCommand command)
+    {
+        // Create a new Item entity
+        var item = new Item
+        {
+            Name = command.Name,
+            Id = CombGuidIdGeneration.NewGuid(),
+            Description = string.Empty
+        };
+
+        return (new ItemCreated(item.Id), Storage.Insert(item));
+    }
 
     [WolverineGet("/api/item/{id}")]
     public static Item Get([Entity] Item item) => item;
