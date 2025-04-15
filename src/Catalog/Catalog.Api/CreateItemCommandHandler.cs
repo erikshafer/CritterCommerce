1092ad2fbc60e56b22c25611a131c6288aac8d0d
Erik Shafer <erik.shafer@gmail.com>
2025-04-14 20:25:40 -0500

    Revert "moved ItemEndpoints to its own file"
    This reverts commit 381c466c9b113cc6eb240e36f0bc4e914c2b2aee.

--- src/Catalog/Catalog.Api/CreateItemCommandHandler.cs
+++ src/Catalog/Catalog.Api/CreateItemCommandHandler.cs
@@ -1,4 +1,7 @@
+using JasperFx.Core;
 using Wolverine.Attributes;
+using Wolverine.Http;
+using Wolverine.Persistence;
 
 namespace Catalog.Api;
 
@@ -25,4 +28,25 @@ public class ItemCreatedHandler
     {
         Console.WriteLine("You created a new item with id " + @event.Id);
     }
+}
+
+public static class ItemEndpoints
+{
+    [Transactional]
+    [WolverinePost("/api/items/create"), AlwaysPublishResponse]
+    public static (ItemCreated, Insert<Item>) Post(CreateItemCommand command)
+    {
+        // Create a new Item entity
+        var item = new Item
+        {
+            Name = command.Name,
+            Id = CombGuidIdGeneration.NewGuid(),
+            Description = string.Empty
+        };
+
+        return (new ItemCreated(item.Id), Storage.Insert(item));
+    }
+
+    [WolverineGet("/api/item/{id}")]
+    public static Item Get([Entity] Item item) => item;
 }
