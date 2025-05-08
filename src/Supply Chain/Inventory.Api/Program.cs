4d164da0c428820188ad06bc662e5b4d5b0c8efb
Erik Shafer <erik.shafer@gmail.com>
2025-05-08 15:15:33 -0500

    setting up Program.cs, new dependencies

--- src/Supply Chain/Inventory.Api/Program.cs
+++ src/Supply Chain/Inventory.Api/Program.cs
@@ -1,6 +1,46 @@
+using JasperFx;
+using JasperFx.Resources;
+using Marten;
+using Wolverine;
+using Wolverine.Http;
+using Wolverine.Marten;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.ApplyJasperFxExtensions();
+
+// Adding Marten for persistence
+builder.Services
+    .AddMarten(opts =>
+    {
+        var connectionString = builder.Configuration.GetConnectionString("marten");
+        opts.Connection(connectionString!);
+        opts.DatabaseSchemaName = "inventory";
+    })
+    // Optionally add Marten/Postgresql integration with Wolverine's outbox
+    .IntegrateWithWolverine();
+
+builder.Services.AddResourceSetupOnStartup();
+
+builder.Host.UseWolverine(opts =>
+{
+    opts.Policies.AutoApplyTransactions();
+    opts.Policies.UseDurableLocalQueues();
+});
+
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddWolverineHttp();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.Run();
+app.MapWolverineEndpoints();
+
+app.MapGet("/", ()
+    => Results.Redirect("/swagger"));
+
+return await app.RunJasperFxCommands(args);
