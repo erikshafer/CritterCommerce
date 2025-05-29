1a1b493a651ba9a00afc04a2a94a190d6affa20e
Erik Shafer <erik.shafer@gmail.com>
2025-05-29 15:27:25 -0500

    GET endpoint for read model (projection)

--- src/Supply Chain/Inventory.Api/QueryInventoryEndpoints.cs
+++ src/Supply Chain/Inventory.Api/QueryInventoryEndpoints.cs
@@ -1,3 +1,5 @@
+using Marten;
+using Microsoft.AspNetCore.Mvc;
 using Wolverine.Http;
 using Wolverine.Persistence;
 
@@ -6,5 +8,19 @@ namespace Inventory.Api;
 public static class QueryInventoryEndpoints
 {
     [WolverineGet("/api/inventory/{id}", Name = "GetInventory")]
-    public static Inventory Get([Entity] Inventory inventory) => inventory;
+    public static Inventory GetEntity([Entity] Inventory inventory) => inventory;
+
+    [WolverineGet("/api/inventory/read-model/{id}",  Name = "GetInventoryReadModel")]
+    [ProducesResponseType(404)]
+    [ProducesResponseType(200, Type = typeof(InventoryReadModel))]
+    public static async Task<IResult> GetReadModel(
+        Guid id,
+        IQuerySession session,
+        CancellationToken ct)
+    {
+        var invoice = await session.LoadAsync<InventoryReadModel>(id, ct);
+        return invoice == null
+            ? Results.NotFound()
+            : Results.Ok(invoice);
+    }
 }
