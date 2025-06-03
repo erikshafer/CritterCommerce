a9f610bbaa58a766790b95973a7a985edc806303
Erik Shafer <erik.shafer@gmail.com>
2025-06-03 08:48:16 -0500

    initial draft of one-file for fulfillment centers and warehouses

---
+++ src/Supply Chain/Inventory/FulfillmentCenters.cs
@@ -0,0 +1,12 @@
+namespace Inventory;
+
+public enum FulfillmentCenterLocation
+{
+    Unset = 0,
+    OmahaNeUsa = 1,
+    AustinTxUsa = 2,
+    BellevueWaUsa = 4,
+    PhiladelphiaPaUsa = 8,
+    TorontoOnCa = 16,
+    DoncasterUk = 32
+}
