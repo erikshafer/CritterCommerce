8a5b06b42393cc96147a8326111f0fa27ee66ae2
Erik Shafer <erik.shafer@gmail.com>
2025-06-04 23:51:18 -0500

    a neat little comment

--- src/Supply Chain/Inventory/Exceptions.cs
+++ src/Supply Chain/Inventory/Exceptions.cs
@@ -2,7 +2,11 @@ namespace Inventory;
 
 public class DomainException(string message) : Exception(message);
 
-// Perhaps someday this will use a shared library, or
-// perhaps it'll become InventoryDomainException so each
-// value stream / department / team has its own.
-// Who knows! ¯\_(ツ)_/¯
+/***
+ *
+ * Perhaps someday this will use a shared library, or
+ * perhaps it'll become InventoryDomainException so each
+ * value stream / department / team has its own.
+ * Who knows! ¯\_(ツ)_/¯
+ *
+ */
