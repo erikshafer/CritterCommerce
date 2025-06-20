420f1f66d42a3875ed91d4021307023d99e06a74
Erik Shafer <erik.shafer@gmail.com>
2025-06-19 23:06:50 -0500

    methods renamed to match GET call names

--- src/Catalog/Legacy.Catalog.Api/Items/QueryItemEndpoints.cs
+++ src/Catalog/Legacy.Catalog.Api/Items/QueryItemEndpoints.cs
@@ -6,10 +6,10 @@ namespace Legacy.Catalog.Api.Items;
 public static class QueryItemEndpoints
 {
     [WolverineGet("/api/items/{id:guid}", Name = "GetItemById")]
-    public static async Task<Item> Get(Guid id, CatalogDbContext db) =>
+    public static async Task<Item> GetItemById(Guid id, CatalogDbContext db) =>
         await db.Items.AsNoTracking().FirstAsync(x => x.Id == id);
 
     [WolverineGet("/api/items", Name = "GetAllItems")]
-    public static async Task<IReadOnlyList<Item>> GetItems(CatalogDbContext db) =>
+    public static async Task<IReadOnlyList<Item>> GetAllItems(CatalogDbContext db) =>
         await db.Items.AsNoTracking().ToListAsync();
 }
