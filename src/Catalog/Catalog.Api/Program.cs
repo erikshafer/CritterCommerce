3cc0937357b67e1639245ca10b3dae7419e27c8e
Erik Shafer <erikshafer@users.noreply.github.com>
2025-06-20 23:46:58 -0500

    Catalog, but with Critter Stack (#15)
    * created the new, modern, catalog projects
    
    * reference
    
    * small, basic scaffold of Marten and Wolverine to the new Catalog projects

---
+++ src/Catalog/Catalog.Api/Program.cs
@@ -0,0 +1,32 @@
+using JasperFx;
+using Marten;
+using Wolverine;
+using Wolverine.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Host.ApplyJasperFxExtensions();
+
+builder.Services.AddMarten(options =>
+{
+    var martenConnectionString = builder.Configuration.GetConnectionString("marten")
+                                 ?? throw new Exception("Marten connection string not found");
+    options.Connection(martenConnectionString!);
+    options.DatabaseSchemaName = "catalog";
+});
+
+builder.Host.UseWolverine();
+
+builder.Services.AddWolverineHttp();
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", (HttpResponse response) =>
+{
+    response.Headers.Append("Location", "/swagger");
+    response.StatusCode = StatusCodes.Status301MovedPermanently;
+}).ExcludeFromDescription();
+
+return await app.RunJasperFxCommands(args);
