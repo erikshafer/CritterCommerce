12259e064a137bda39433ddcc5e956cd4e485265
Erik Shafer <erik.shafer@gmail.com>
2025-06-20 23:44:37 -0500

    small, basic scaffold of Marten and Wolverine to the new Catalog projects

--- src/Catalog/Catalog.Api/Program.cs
+++ src/Catalog/Catalog.Api/Program.cs
@@ -1,6 +1,32 @@
+using JasperFx;
+using Marten;
+using Wolverine;
+using Wolverine.Http;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Host.ApplyJasperFxExtensions();
+
+builder.Services.AddMarten(options =>
+{
+    var martenConnectionString = builder.Configuration.GetConnectionString("marten")
+                                 ?? throw new Exception("Marten connection string not found");
+    options.Connection(martenConnectionString!);
+    options.DatabaseSchemaName = "catalog";
+});
+
+builder.Host.UseWolverine();
+
+builder.Services.AddWolverineHttp();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.Run();
+app.MapGet("/", (HttpResponse response) =>
+{
+    response.Headers.Append("Location", "/swagger");
+    response.StatusCode = StatusCodes.Status301MovedPermanently;
+}).ExcludeFromDescription();
+
+return await app.RunJasperFxCommands(args);
