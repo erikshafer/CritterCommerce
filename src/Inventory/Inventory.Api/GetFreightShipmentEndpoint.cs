e0c2ddb8282f3522673cbf68da9d127096066b55
Erik Shafer <erik.shafer@gmail.com>
2025-06-24 21:03:20 -0500

    separated endpoints

---
+++ src/Inventory/Inventory.Api/GetFreightShipmentEndpoint.cs
@@ -0,0 +1,18 @@
+using Inventory.Inbound;
+using Marten;
+using Marten.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+using Wolverine.Http;
+
+namespace Inventory.Api;
+
+public static class GetFreightShipmentEndpoint
+{
+    // For right now, you have to help out the OpenAPI metadata
+    [Produces(typeof(FreightShipment))]
+    [WolverineGet("/api/freight-shipments/{id}")]
+    public static async Task Get(Guid id, IDocumentSession session, HttpContext context)
+    {
+        await session.Json.WriteById<FreightShipment>(id, context);
+    }
+}
--- src/Inventory/Inventory.Api/ScheduleFreightShipmentEndpoint.cs
+++ src/Inventory/Inventory.Api/ScheduleFreightShipmentEndpoint.cs
@@ -1,22 +1,9 @@
 using Inventory.Inbound;
-using Marten;
-using Marten.AspNetCore;
 using Microsoft.AspNetCore.Mvc;
 using Wolverine.Http;
 using Wolverine.Marten;
 
 namespace Inventory.Api;
-
-public static class GetFreightShipmentEndpoint
-{
-    // For right now, you have to help out the OpenAPI metadata
-    [Produces(typeof(FreightShipment))]
-    [WolverineGet("/api/freight-shipments/{id}")]
-    public static async Task Get(Guid id, IDocumentSession session, HttpContext context)
-    {
-        await session.Json.WriteById<FreightShipment>(id, context);
-    }
-}
 
 public static class ScheduleFreightShipmentEndpoint
 {
@@ -42,4 +29,3 @@ public static class ScheduleFreightShipmentEndpoint
         return (response, start);
     }
 }
-
