29e1e47effb7d877d1503db676b67a0b9e25fef1
Erik Shafer <erik.shafer@gmail.com>
2025-07-06 23:47:55 -0500

    (FIX) due to downgrading OpenApi dependency

--- src/Inventory/Inventory.Api/Program.cs
+++ src/Inventory/Inventory.Api/Program.cs
@@ -128,7 +128,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
