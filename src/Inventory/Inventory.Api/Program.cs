e60f93204240951a6889da9b5fecd9c22e0b0b43
Erik Shafer <erik.shafer@gmail.com>
2025-07-06 23:50:03 -0500

    (FIX) versions bumped AND corrected

--- src/Inventory/Inventory.Api/Program.cs
+++ src/Inventory/Inventory.Api/Program.cs
@@ -128,6 +128,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
