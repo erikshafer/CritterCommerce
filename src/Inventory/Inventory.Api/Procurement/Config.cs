310b04f8f71c4f4aaa30f9eca2db0cb77442fef9
Erik Shafer <erik.shafer@gmail.com>
2025-07-21 18:20:56 -0500

    added Config files for each module to set-up registration of projections, services, etc.

---
+++ src/Inventory/Inventory.Api/Procurement/Config.cs
@@ -0,0 +1,15 @@
+using Marten;
+
+namespace Inventory.Api.Procurement;
+
+internal static class Config
+{
+    internal static void AddProcurementDocuments(this StoreOptions opts)
+    {
+        opts.RegisterDocumentType<ProcurementOrder>();
+        opts.Schema.For<ProcurementOrder>()
+            .Identity(x => x.Id)
+            .Duplicate(x => x.VendorId) // Consider making this a foreign key to the Vendor docs
+            .Duplicate(x => x.TrackingNumber); // Could add the entire document's properties here, but
+    }
+}
--- src/Inventory/Inventory.Api/Program.cs
+++ src/Inventory/Inventory.Api/Program.cs
@@ -1,21 +1,16 @@
 using System.Text.Json.Serialization;
 using Inventory.Api;
 using Inventory.Api.Inbound;
-using Inventory.Api.Inbound.Views;
 using Inventory.Api.Locations;
 using Inventory.Api.Procurement;
 using Inventory.Api.Receiving;
-using Inventory.Api.Receiving.Views;
 using Inventory.Api.Vendors;
 using Inventory.Api.WarehouseLevels;
-using Inventory.Api.WarehouseLevels.Lots;
 using JasperFx;
 using JasperFx.Core;
 using JasperFx.Events.Daemon;
-using JasperFx.Events.Projections;
 using JasperFx.Resources;
 using Marten;
-using Marten.Events.Projections;
 using Wolverine;
 using Wolverine.ErrorHandling;
 using Wolverine.FluentValidation;
@@ -39,54 +34,15 @@ builder.Services.AddMarten(opts =>
         opts.DatabaseSchemaName = Constants.Inventory;
         opts.DisableNpgsqlLogging = true;
 
-        // The inline projections, with snapshots.
-        // With every commit, such as appending an event, updating all associated
-        // projections will be batched in a single call to the Postgres database.
-        // However, you sacrifice some event metadata usage by doing this.
-        opts.Projections
-            .Snapshot<FreightShipment>(SnapshotLifecycle.Inline)
-            .Identity(x => x.Id)
-            .Duplicate(x => x.Origin)
-            .Duplicate(x => x.Destination);
+        // Add projections
+        opts.AddWarehouseProjections();
+        opts.AddReceivingProjections();
+        opts.AddFreightProjections();
 
-        opts.Projections
-            .Snapshot<ReceivedShipment>(SnapshotLifecycle.Inline)
-            .Identity(x => x.Id);
-
-        opts.Projections
-            .Snapshot<InventoryLevel>(SnapshotLifecycle.Inline)
-            .Identity(x => x.Id)
-            .Duplicate(x => x.Sku);
-
-        // The async projections with snapshotting.
-        // An async daemon will be running in the background, which yes it can be
-        // configured and tweaked, and will process all registered projections
-        // associated with what has recently been appended to the event store in PostgreSQL.
-        // Docs for async daemon: https://martendb.io/events/projections/async-daemon.html#async-projections-daemon
-        opts.Projections.Add<ExpectedQuantityAnticipatedProjection>(ProjectionLifecycle.Async);
-        opts.Projections.Add<DailyShipmentsProjection>(ProjectionLifecycle.Async);
-        opts.Projections.Add<FreightShipmentProjection>(ProjectionLifecycle.Async);
-        opts.Projections.Add<WarehouseLotsProjection>(ProjectionLifecycle.Async);
-
-        opts.RegisterDocumentType<Location>();
-        opts.Schema.For<Location>()
-            .Identity(x => x.Id)
-            .Duplicate(x => x.Name);
-
-        opts.RegisterDocumentType<Vendor>();
-        opts.Schema.For<Vendor>()
-            .Identity(x => x.Id)
-            .Duplicate(x => x.Name);
-
-        opts.RegisterDocumentType<ProcurementOrder>();
-        opts.Schema.For<ProcurementOrder>()
-            .Identity(x => x.Id)
-            .Duplicate(x => x.VendorId) // Consider making this a foreign key to the Vendor docs
-            .Duplicate(x => x.TrackingNumber); // Could add the entire document's properties here, but
-
-        opts.Schema.For<WarehouseLotsView>()
-            .Duplicate(x => x.Warehouse)
-            .Duplicate(x => x.Lot);
+        // Add documents
+        opts.AddLocationDocuments();
+        opts.AddVendorDocuments();
+        opts.AddProcurementDocuments();
     })
     .InitializeWith(new InitialData(
         InitialData.ConcatDataSets(
@@ -138,7 +94,8 @@ builder.Host.UseWolverine(opts =>
         .UseDurableOutbox();
 });
 
-builder.Services.AddSingleton<IFacilityLotService, FacilityLotService>();
+// Add services (domain, etc)
+builder.Services.AddWarehouseServices();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
